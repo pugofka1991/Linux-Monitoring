5cba36ed4af4c2dacf7f7ebb2139b011e20f0cc5
thadachi <pugofka@mail.ru>
2022-07-29 14:58:56 +0300

    03

--- src/03/main.sh
+++ src/03/main.sh
@@ -5,6 +5,11 @@ then
     echo "You entered the wrong number of arguments, please try again"
     echo "enter 4 parameters from 1 - 6"
 else
+    export backNAME=$1
+    export textNAME=$2
+    export backTEXT=$3
+    export textTEXT=$4
+    
     chmod +x info.sh
     ./info.sh
 fi
